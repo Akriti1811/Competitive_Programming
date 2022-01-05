8eecf265aa493cf35df14ad0921bf6300d0e2180
Akriti <akritisingh1811@gmail.com>
2022-01-05 17:13:38 +0530

    Subtraction game 1 (5th Jan)

---
+++ subtraction_game1_codechef.cpp
@@ -0,0 +1,51 @@
+/*
+Chef is playing a game on a sequence of N positive integers, say A1, A2, ... AN. The game is played as follows.
+
+If all the numbers are equal, the game ends.
+Otherwise
+Select two numbers which are unequal
+Subtract the smaller number from the larger number
+Replace the larger number with the result from above (see the explanation section for clarity)
+Chef has already figured out that the game always terminates. He also knows, for a given sequence of integers, the game will always terminate on the same value, no matter how the game is played. Chef wants you to simulate the game for him and tell him on which value will the game terminate for a given sequence of integers.
+
+Input
+The first line of the input contains an integer T, the number of test cases. Then follow the description of T test cases. The first line of each test case contains a single integer N, the length of the sequence. The second line contains N positive integers, each separated by a single space.
+
+Output
+For each test case, output a single integer - the value of all the numbers when they are equal (and the game terminates), on a line by itself.
+
+Constraints
+1 ≤ T ≤ 100
+1 ≤ N ≤ 1000
+1 ≤ Ai ≤ 109
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b)
+{
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+int main() 
+{
+    int t;
+    cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    int p=a[0];
+	    for(int i=1;i<n;i++)
+	    {
+	        p=gcd(p,a[i]);
+	    }
+	    cout<<p<<"\n";
+	}
+	return 0;
+}
