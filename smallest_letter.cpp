80dff68bbc6177d0f7d3395cf7511418457377cd
Akriti <akritisingh1811@gmail.com>
2022-01-11 21:07:05 +0530

    Find Smallest Letter Greater Than Target

---
+++ smallest_letter.cpp
@@ -0,0 +1,29 @@
+/*
+Find Smallest Letter Greater Than Target
+Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+Note that the letters wrap around.
+For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+Example 1:
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Example 2:
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+*/
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) 
+    {
+        int n=letters.size();
+        if(target>=letters[n-1])
+            target=' ';
+       
+        for(int i=0;i<n;i++)
+        {
+            if(letters[i]>target)
+                return letters[i];
+        }
+        return ' ';
+    }
+};
