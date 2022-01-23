6dd6f6736f60b83c339b5abd6258053f29f691cf
Akriti <akritisingh1811@gmail.com>
2022-01-24 00:13:08 +0530

    Final Value of Variable After Performing Operations (23rd Jan)

---
+++ variable_value.cpp
@@ -0,0 +1,42 @@
+/*
+Final Value of Variable After Performing Operations
+There is a programming language with only four operations and one variable X:
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+Example 1:
+Input: operations = ["X++","++X","--X","X--"]
+Output: 0
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+X++: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+--X: X is decremented by 1, X = 2 - 1 = 1.
+X--: X is decremented by 1, X = 1 - 1 = 0.
+Constraints:
+1 <= operations.length <= 100
+operations[i] will be either "++X", "X++", "--X", or "X--".
+*/
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& op) 
+    {
+        int x=0;
+        for(int i=0;i<op.size();i++)
+        {
+            if(op[i].compare("X++")==0)
+                x++;
+            else if(op[i].compare("++X")==0)
+                ++x;
+            else if(op[i].compare("X--")==0)
+                x--;
+            else if(op[i].compare("--X")==0)
+                --x;
+            else
+                continue;
+        }
+        return x;
+    }
+};
